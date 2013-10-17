2cbf7ffbbe721d3d580b5a2d4e55b5b4a27ba115
Ross Hanson <ross.hanson@gmail.com>
2013-10-17 18:53:20 -0400

    Modified .gitignore and tested compilation

---
+++ src/jvr/engine/Engine.java
@@ -0,0 +1,14 @@
+package jvr.engine;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ross
+ * Date: 10/17/13
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Engine {
+    public static void main(String args[]){
+        System.out.println("Hey! I compiled! Add some stuff");
+    }
+}
