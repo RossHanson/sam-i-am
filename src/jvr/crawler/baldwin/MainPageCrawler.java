cd150dc21896b63aee55e3009a0e2aea42691164
Ross Hanson <ross.hanson@gmail.com>
2013-10-27 14:38:48 -0400

    Minor change toTesting parser import

---
+++ src/jvr/crawler/baldwin/MainPageCrawler.java
@@ -0,0 +1,11 @@
+package jvr.crawler.baldwin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ross
+ * Date: 10/27/13
+ * Time: 2:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainPageCrawler {
+}
