1b367a4b2aba0965809744300c5bd45205ff5fb8
Ross Hanson <ross.hanson@gmail.com>
2013-12-03 16:41:07 -0500

    Updated a bunch of smaller stuff

--- src/jvr/crawler/baldwin/MainPageCrawler.java
+++ src/jvr/crawler/baldwin/MainPageCrawler.java
@@ -19,16 +19,21 @@ import java.util.Iterator;
  */
 public class MainPageCrawler {
 
+    private static int LIMIT = 200;
 
     private static String PAGE_URL = "http://www.mainlesson.com/displaystoriesbytitle.php";
+    private static String HEADLINE_SELECTOR = "body table tbody tr td table tbody tr td:nth-child(1) a";
 
     public static void main(String args[]) throws IOException, ParserConfigurationException {
+        int counter = 0;
         Document titlePage = readPage(PAGE_URL);
-        Elements newsHeadlines = titlePage.select("body table tbody tr td table tbody tr td:nth-child(1) a");
-        for (Iterator<Element> elements = newsHeadlines.iterator(); elements.hasNext();){
+        Elements newsHeadlines = titlePage.select(HEADLINE_SELECTOR);
+        for (Iterator<Element> elements = newsHeadlines.iterator(); counter <= LIMIT && elements.hasNext();){
             System.out.println("Element is :" + elements.next().attr("href"));
-            StoryScraper story = new StoryScraper(elements.next().attr("href"));
+            String targ = elements.next().attr("href");
+            StoryScraper story = new StoryScraper("data/" + StoryScraper.parseFName(targ),targ);
             story.downloadFile();
+            counter++;
         }
     }
 
